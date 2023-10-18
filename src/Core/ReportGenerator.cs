df93820868b5019e1d2e9bfbbdb8e902bce466f0
Stian Hanger <pdnagilum@gmail.com>
2023-10-18 23:32:50 +0200

    Sort URLs in report

--- src/Core/ReportGenerator.cs
+++ src/Core/ReportGenerator.cs
@@ -140,7 +140,7 @@ internal class ReportGenerator
         var culture = new CultureInfo("en-US");
         var sb = new StringBuilder();
 
-        foreach (var entry in this._queue.Where(n => n.UrlType == urlType))
+        foreach (var entry in this._queue.Where(n => n.UrlType == urlType).OrderBy(n => n.Url))
         {
             string cssClass;
             
